3b92573e4e114e24077d968e538385863a621365
Tomas Cortina <tomascortina00@uca.edu.ar>
2026-09-14 03:21:38 -0300

    chore(config): transform bcrypt-ts in jest and add jest/node types

--- jest.config.ts
+++ jest.config.ts
@@ -7,6 +7,7 @@ const config: Config = {
     testEnvironment: 'node',
     testMatch: ['<rootDir>/tests/unit/**/*.test.ts'],
     moduleNameMapper: { '^@/(.*)$': '<rootDir>/$1' },
+    transformIgnorePatterns: ['/node_modules/(?!bcrypt-ts)'],
 };
 
 export default createJestConfig(config);
